c3077e9dad801f8287706754fcf9c7cc04f4db8a
srgfz <fernandezsergio10@gmail.com>
2022-11-07 20:41:47 +0100

    Add king movement

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ casillas.forEach((fila, index) => {
 let turnoBlancas = true;//True será white y false será black
 let piezaSeleccionada
 let posicionPiezaSeleccionada
+let posicionComprobacionMovimientoRey
 
 const cargarPiezas = () => {
     let primeraFila = tablero.firstElementChild
@@ -66,6 +67,7 @@ seleccionarPieza = (pieza = null) => {
                 casilla.classList.remove("seleccionada")
                 casilla.classList.remove("posibleDestinoVacio")
                 casilla.classList.remove("posibleDestinoRival")
+                casilla.classList.remove("futuroDestinoPosible")
             } else {
                 piezaSeleccionada = casilla
             }
@@ -77,16 +79,23 @@ seleccionarPieza = (pieza = null) => {
     }
 }
 
-marcarPosicionesPosibles = (casillasPosiblesVacias, casillasPosiblesEnemigas) => {
-    //Marco las posiciones posibles vacias
-    casillasPosiblesVacias.forEach(casilla => {
-        casilla.textContent = "\u{25CF}"
-        casilla.classList.add("posibleDestinoVacio")
-    })
-    //Marco las posiciones con piezas rivales posibles
-    casillasPosiblesEnemigas.forEach(casilla => {
-        casilla.classList.add("posibleDestinoRival")
-    })
+marcarPosicionesPosibles = (casillasPosiblesVacias, casillasPosiblesEnemigas, comprobacionRey = false) => {
+    if (!comprobacionRey) {//Si no es una comprobación de los movimientos del Rey
+        //Marco las posiciones posibles vacias
+        casillasPosiblesVacias.forEach(casilla => {
+            casilla.textContent = "\u{25CF}"
+            casilla.classList.add("posibleDestinoVacio")
+        })
+        //Marco las posiciones con piezas rivales posibles
+        casillasPosiblesEnemigas.forEach(casilla => {
+            casilla.classList.add("posibleDestinoRival")
+        })
+    } else {//Si es una comprobación de los movimientos del Rey
+        casillasPosiblesVacias.forEach(casilla => {
+            casilla.classList.add("futuroDestinoPosible")
+        })
+    }
+
 }
 
 moverPieza = (destino) => {
@@ -112,13 +121,13 @@ moverPieza = (destino) => {
     seleccionarPieza()//Reseteo las clases de las casillas
 }
 
-movimientoPeon = (piezaEnemiga, posicionBase, movimientoFrontal, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
-    if ((posicionPiezaSeleccionada[0] + movimientoFrontal <= 7) && (posicionPiezaSeleccionada[0] + movimientoFrontal >= 0)) {//Si la casilla a comprobar existe
-        casillaComprobar = casillas[posicionPiezaSeleccionada[0] + movimientoFrontal][posicionPiezaSeleccionada[1]]
+movimientoPeon = (piezaEnemiga, posicionBase, movimientoFrontal, posicionOrigen = posicionPiezaSeleccionada, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
+    if ((posicionOrigen[0] + movimientoFrontal <= 7) && (posicionOrigen[0] + movimientoFrontal >= 0)) {//Si la casilla a comprobar existe
+        casillaComprobar = casillas[posicionOrigen[0] + movimientoFrontal][posicionOrigen[1]]
         if (casillaComprobar.textContent === "") {
             casillasPosiblesVacias.push(casillaComprobar)
-            if (posicionPiezaSeleccionada[0] === posicionBase && casillas[posicionPiezaSeleccionada[0] + movimientoFrontal * 2][posicionPiezaSeleccionada[1]].textContent === "") {//Si el peón está en la posición de inicio y la siguiente también está vacía puede avanzar 2
-                casillasPosiblesVacias.push(casillas[posicionPiezaSeleccionada[0] + movimientoFrontal * 2][posicionPiezaSeleccionada[1]])
+            if (posicionOrigen[0] === posicionBase && casillas[posicionOrigen[0] + movimientoFrontal * 2][posicionOrigen[1]].textContent === "") {//Si el peón está en la posición de inicio y la siguiente también está vacía puede avanzar 2
+                casillasPosiblesVacias.push(casillas[posicionOrigen[0] + movimientoFrontal * 2][posicionOrigen[1]])
             }
         }
         if (casillaComprobar.nextElementSibling != undefined && casillaComprobar.nextElementSibling.classList.contains(piezaEnemiga)) {//Si existe la casilla diagonal derecha y tiene una pieza enemiga
@@ -131,18 +140,21 @@ movimientoPeon = (piezaEnemiga, posicionBase, movimientoFrontal, casillasPosible
     marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas)
 }
 
-movimientoLineal = (piezaEnemiga, movimientoFrontal = 0, movimientoLateral = 0, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
+movimientoLineal = (piezaEnemiga, movimientoFrontal = 0, movimientoLateral = 0, movimientoRey = false, comprobacionRey = false, posicionOrigen = posicionPiezaSeleccionada, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
     posicionDisponible = true
-    while (posicionDisponible && ((posicionPiezaSeleccionada[0] + movimientoFrontal <= 7) && (posicionPiezaSeleccionada[0] + movimientoFrontal >= 0) && (posicionPiezaSeleccionada[1] + movimientoLateral <= 7) && (posicionPiezaSeleccionada[1] + movimientoLateral >= 0))) {//Mientras no encuentre una casilla ocupada y la casilla exista en el tablero
-        casillaComprobar = casillas[posicionPiezaSeleccionada[0] + movimientoFrontal][posicionPiezaSeleccionada[1] + movimientoLateral]
+    while (posicionDisponible && ((posicionOrigen[0] + movimientoFrontal <= 7) && (posicionOrigen[0] + movimientoFrontal >= 0) && (posicionOrigen[1] + movimientoLateral <= 7) && (posicionOrigen[1] + movimientoLateral >= 0))) {//Mientras no encuentre una casilla ocupada y la casilla exista en el tablero
+        casillaComprobar = casillas[posicionOrigen[0] + movimientoFrontal][posicionOrigen[1] + movimientoLateral]
         if (casillaComprobar.textContent === "") {//Si la posición está vacía
             casillasPosiblesVacias.push(casillaComprobar)
-            //******llamar aquí a la función del peon: si está en la posición base comprobar la siguiente */
         } else {//Si no está vacía será la última casilla que comprobará
             posicionDisponible = false
             if (casillaComprobar.classList.contains(piezaEnemiga)) {//Si contiene una pieza enemiga
                 casillasPosiblesEnemigas.push(casillaComprobar)
             }
+        }
+        if (movimientoRey) {//Si se trata del movimiento del rey
+            posicionDisponible = false
+            comprobarCasillasRey(piezaEnemiga)
         }
         //Aumento el contador que va comprobando las celdas
         if (movimientoFrontal > 0) {
@@ -156,21 +168,97 @@ movimientoLineal = (piezaEnemiga, movimientoFrontal = 0, movimientoLateral = 0, 
             movimientoLateral--
         }
     }
-    marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas)
+    // if (comprobacionRey) {
+    //     casillasPosiblesVacias = eliminarPosiblesCasillasRey(casillasPosiblesVacias)
+    //     casillasPosiblesEnemigas = eliminarPosiblesCasillasRey(casillasPosiblesEnemigas)
+    // }
+    // console.log("***********")
+    // console.log(movimientoRey, comprobacionRey)
+    // console.log(movimientoFrontal)
+    // console.log("***********")
+    if (movimientoRey && comprobacionRey) {
+        casillasPosiblesVacias = eliminarPosiblesCasillasRey(casillasPosiblesVacias)
+        casillasPosiblesEnemigas = eliminarPosiblesCasillasRey(casillasPosiblesEnemigas)
+        comprobacionRey = false
+    }
+    marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas, comprobacionRey)
 }
 
-movimientoCaballo = (piezaEnemiga, desplazamientoFrontal, desplazamientoLateral, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
-    if ((posicionPiezaSeleccionada[0] + desplazamientoFrontal <= 7) && (posicionPiezaSeleccionada[0] + desplazamientoFrontal >= 0) && (posicionPiezaSeleccionada[1] + desplazamientoLateral <= 7) && (posicionPiezaSeleccionada[1] + desplazamientoLateral >= 0)) {
-        casillaComprobar = casillas[posicionPiezaSeleccionada[0] + desplazamientoFrontal][posicionPiezaSeleccionada[1] + desplazamientoLateral]
+movimientoCaballo = (piezaEnemiga, desplazamientoFrontal, desplazamientoLateral, comprobacionRey = false, posicionOrigen = posicionPiezaSeleccionada, casillasPosiblesVacias = [], casillasPosiblesEnemigas = []) => {
+    if ((posicionOrigen[0] + desplazamientoFrontal <= 7) && (posicionOrigen[0] + desplazamientoFrontal >= 0) && (posicionOrigen[1] + desplazamientoLateral <= 7) && (posicionOrigen[1] + desplazamientoLateral >= 0)) {
+        casillaComprobar = casillas[posicionOrigen[0] + desplazamientoFrontal][posicionOrigen[1] + desplazamientoLateral]
         if (casillaComprobar.textContent === "") {//Si la casilla está vacía
             casillasPosiblesVacias.push(casillaComprobar)
         } else if (casillaComprobar.classList.contains(piezaEnemiga)) {//Si la casilla tiene una pieza enemiga
             casillasPosiblesEnemigas.push(casillaComprobar)
         }
-        marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas)
     }
-    marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas)
+    marcarPosicionesPosibles(casillasPosiblesVacias, casillasPosiblesEnemigas, comprobacionRey)
 }
+
+comprobarCasillasRey = (piezaEnemiga) => {
+    let posicionComprobar
+    //Recorro todo el tablero buscando piezas Enemigas para comprobar sus posibles movimientos en el siguiente turno
+    casillas.forEach(fila => {
+        fila.forEach(casilla => {//Marco todos los posibles movimientos enemigos del proximo turno con la clase ".futuroDestinoPosible"
+            if (casilla.classList.contains(piezaEnemiga)) {//Si la casilla tiene una pieza enemiga
+                posicionComprobar = posicionPieza(casilla)//Actualizo la posición de la pieza seleccionada a la pieza de la que voy a ir haciendo la comprobación
+                if (casilla.textContent === knight) {//comprobación del caballo
+                    movimientoCaballo(piezaEnemiga, 2, 1, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, 2, -1, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, -2, 1, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, -2, -1, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, 1, 2, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, 1, -2, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, -1, 2, true, posicionComprobar)
+                    movimientoCaballo(piezaEnemiga, -1, -2, true, posicionComprobar)
+                } else if (casilla.textContent === rook) {//comprobación de la torre
+                    movimientoLineal(piezaEnemiga, 1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, -1, false, true, posicionComprobar)
+                } else if (casilla.textContent === bishop) {//comprobación del alfil
+                    movimientoLineal(piezaEnemiga, 1, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 1, false, true, posicionComprobar)
+                } else if (casilla.textContent === queen) {//comprobación de la reina
+                    //Movimientos laterales/frontales
+                    movimientoLineal(piezaEnemiga, 1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, -1, false, true, posicionComprobar)
+                    //Movimientos diagonales
+                    movimientoLineal(piezaEnemiga, 1, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 1, false, true, posicionComprobar)
+                } else if (casilla.textContent === king) {//comprobación del rey
+                    movimientoLineal(piezaEnemiga, 1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 0, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 0, -1, false, true, posicionComprobar)
+                    //Movimientos diagonales
+                    movimientoLineal(piezaEnemiga, 1, 1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, 1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, -1, false, true, posicionComprobar)
+                    movimientoLineal(piezaEnemiga, -1, 1, false, true, posicionComprobar)
+                }
+            }
+        })
+    })
+}
+
+eliminarPosiblesCasillasRey = (casillasComprobadas = []) => {
+    casillasComprobadas.forEach((casilla, index) => {
+        if (casilla.classList.contains("futuroDestinoPosible")) {//Si el destino no es seguro para el rey lo elimino de posibles destinos
+            casillasComprobadas.splice(index, 1)
+        }
+    })
+    return casillasComprobadas
+
+}
+
 
 controladorCasillasPosibles = (pieza) => {
     casillasPosiblesVacias = []
@@ -178,7 +266,6 @@ controladorCasillasPosibles = (pieza) => {
     piezaEnemiga = turnoBlancas ? "piezaNegra" : "piezaBlanca" //condicional ternario para qué pieza es la enemiga en cada turno
     posicionPiezaSeleccionada = posicionPieza(pieza)
     if (pieza.textContent === pawn) {//Peón
-        console.log("peón")
         if (turnoBlancas) {
             posicionBasePeon = 6
             movimientoFrontal = -1
@@ -188,14 +275,12 @@ controladorCasillasPosibles = (pieza) => {
         }
         movimientoPeon(piezaEnemiga, posicionBasePeon, movimientoFrontal)
     } else if (pieza.textContent === rook) {//Torre
-        console.log("torre")
         //Movimientos Laterales/frontales
         movimientoLineal(piezaEnemiga, 1, 0)
         movimientoLineal(piezaEnemiga, -1, 0)
         movimientoLineal(piezaEnemiga, 0, 1)
         movimientoLineal(piezaEnemiga, 0, -1)
     } else if (pieza.textContent === knight) {//Caballo
-        console.log("caballo")
         movimientoCaballo(piezaEnemiga, 2, 1)
         movimientoCaballo(piezaEnemiga, 2, -1)
         movimientoCaballo(piezaEnemiga, -2, 1)
@@ -210,21 +295,28 @@ controladorCasillasPosibles = (pieza) => {
         movimientoLineal(piezaEnemiga, 1, -1)
         movimientoLineal(piezaEnemiga, -1, -1)
         movimientoLineal(piezaEnemiga, -1, 1)
-        console.log("alfil")
-    } else if (pieza.textContent === queen) {//Dama
-        console.log("dama")
+    } else if (pieza.textContent === queen) {//Reina
         //Movimientos laterales/frontales
         movimientoLineal(piezaEnemiga, 1, 0)
         movimientoLineal(piezaEnemiga, -1, 0)
         movimientoLineal(piezaEnemiga, 0, 1)
         movimientoLineal(piezaEnemiga, 0, -1)
-        //Movimientos diagonales       movimientoLineal(piezaEnemiga, 1, 0)
+        //Movimientos diagonales
         movimientoLineal(piezaEnemiga, 1, 1)
         movimientoLineal(piezaEnemiga, 1, -1)
         movimientoLineal(piezaEnemiga, -1, -1)
         movimientoLineal(piezaEnemiga, -1, 1)
     } else if (pieza.textContent === king) {//Rey
-        console.log("rey")
+        //Movimientos laterales/frontales
+        movimientoLineal(piezaEnemiga, 1, 0, true, true)
+        movimientoLineal(piezaEnemiga, -1, 0, true, true)
+        movimientoLineal(piezaEnemiga, 0, 1, true, true)
+        movimientoLineal(piezaEnemiga, 0, -1, true, true)
+        //Movimientos diagonales
+        movimientoLineal(piezaEnemiga, 1, 1, true, true)
+        movimientoLineal(piezaEnemiga, 1, -1, true, true)
+        movimientoLineal(piezaEnemiga, -1, -1, true, true)
+        movimientoLineal(piezaEnemiga, -1, 1, true, true)
     }
 }
 
@@ -242,7 +334,6 @@ controladorJuego = (ev) => {
         }
     }
     if (casilla.classList.contains("posibleDestinoVacio") || casilla.classList.contains("posibleDestinoRival")) {//Si pulsa sobre la casilla de destino
-        console.log("destino")
         moverPieza(casilla)
     }
 
